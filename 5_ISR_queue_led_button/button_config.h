a9606f64e979ce60061f088c97cfc908430a57d5
skillgarage <vastaplus@gmail.com>
2026-06-04 17:05:39 +0200

    Delete 5_ISR_queue_led_button directory

--- 5_ISR_queue_led_button/button_config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "driver/gpio.h"
-
-#define BTN GPIO_NUM_7
-
-void button_init(void);
--- 5_ISR_queue_led_button/led_config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "driver/gpio.h"
-#include <stdbool.h>
-
-#define LED_GPIO GPIO_NUM_6
-
-void led_init(void);
-void led_set(bool on);
